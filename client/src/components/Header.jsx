78d5a0009271e945ed680dcfe7e52a64f4b86311
Radhi Hudijan <radhif2009@gmail.com>
2024-07-10 12:40:16 +0200

    update the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,46 +1,47 @@
-import { FaSearch } from "react-icons/fa";
-import Singnin from "../pages/Signin";
-import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <header className='bg-slate-200'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-          <span className='text-slate-500'>NOOR</span>
-          <span className='text-slate-700'>ESTATE</span>
-        </h1>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center justify-between'>
+    <header className="bg-slate-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">NOOR</span>
+            <span className="text-slate-700">ESTATE</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center justify-between">
           <input
-            type='text'
-            placeholder='Search ...'
-            className='focus:outline-none bg-transparent w-24 sm:w-64'
+            type="text"
+            placeholder="Search ..."
+            className="focus:outline-none bg-transparent w-24 sm:w-64"
           />
-          <FaSearch className='text-slate-600' />
+          <FaSearch className="text-slate-600" />
         </form>
 
         <nav>
-          <ul className='flex gap-4'>
-            <Link to='/'>
-              <li className='hidden sm:inline text-slate-700 hover:underline'>
+          <ul className="flex gap-4">
+            <Link to="/">
+              <li className="hidden sm:inline text-slate-700 hover:underline">
                 Home
               </li>
             </Link>
 
-            <Link to='/about'>
-              <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <Link to="/about">
+              <li className="hidden sm:inline text-slate-700 hover:underline">
                 About
               </li>
             </Link>
 
-            <Link to='/signin'>
-              <li>Sign In</li>
+            <Link to="/signin">
+              <li className="text-slate-700 hover:underline">Sign In</li>
             </Link>
           </ul>
         </nav>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
