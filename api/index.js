7f346382aae1d826de4c8cf52c231d5f224117f1
Radhi Hudijan <radhif2009@gmail.com>
2024-07-10 13:45:29 +0200

    connecting the DB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,29 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+//Load env variables
+dotenv.config()
 
 //Create an express app
 const app = express()
 
-app.listen(3000, () => {
+//connect to MongoDB
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB)
+    console.log("MongoDB is connected")
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected")
+})
+
+//connect to server
+app.listen(process.env.PORT, () => {
+  connect()
   console.log("Server is running on port 3000")
 })
