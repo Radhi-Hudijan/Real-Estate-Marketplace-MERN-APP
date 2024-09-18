6308d504f8bbf0f57fd665bd5295d62e751b70db
Radhi Hudijan <radhif2009@gmail.com>
2024-09-18 14:40:14 +0200

    feat: Add SignIn page with form handling and error management

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,8 +1,88 @@
-import React from 'react'
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 function Signin() {
+  const [formData, setFormData] = useState({}) // store user input
+  const [error, setError] = useState(null) // store error message
+  const [loading, setLoading] = useState(false) // store loading state
+  const navigate = useNavigate()
+  // handle user input
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value }) // update formData
+  }
+
+  // handle form submission
+  const onSubmit = async (e) => {
+    e.preventDefault() // prevent page refresh
+    try {
+      setLoading(true) // set loading state
+
+      // send POST request to server
+      const response = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData), // send formData
+      })
+
+      const data = await response.json() // get JSON response
+
+      // handle response
+      if (data.success === false) {
+        setLoading(false) // set loading state
+        setError(data.message) // set error message
+        return
+      }
+      setLoading(false) // set loading
+      setError(null) // clear error message
+      alert("Logged in  successfully") // show success message
+      navigate("/") // redirect to login page
+    } catch (error) {
+      setLoading(false) // set loading state
+      setError(error.message) // set error message
+    }
+  }
+
   return (
-    <div>Signin</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col items-center" onSubmit={onSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          className="border border-gray-300 rounded-md p-3 w-80 my-2"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border border-gray-300 rounded-md p-3 w-80 my-2"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading} // disable button when loading
+          type="submit"
+          className="bg-slate-700 text-white w-80 p-3 rounded-md my-2 uppercase hover:bg-slate-800 
+          disabled:opacity-50 
+          cursor-pointer"
+        >
+          {loading ? "LOADING..." : "Sign In"}
+        </button>
+      </form>
+      {error && <p className="text-red-500 text-center mt-3">{error}</p>}
+
+      <div>
+        <p className="text-center mt-3">
+          Do not have an account?{" "}
+          <Link to="/signup" className="text-blue-700 hover:underline">
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
