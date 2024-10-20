ed5b263ab784499692ab9e42b98262acff06c88d
Radhi Hudijan <radhif2009@gmail.com>
2024-10-20 11:39:29 +0200

    Add createListing functionality to listing controller

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
+import listingRouter from "./routes/listing.route.js"
 import cookieParser from "cookie-parser"
 
 //Load env variables
@@ -34,6 +35,7 @@ mongoose.connection.on("disconnected", () => {
 //Routes
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
+app.use("/api/listing", listingRouter)
 
 //error handling middleware
 app.use((err, req, res, next) => {
