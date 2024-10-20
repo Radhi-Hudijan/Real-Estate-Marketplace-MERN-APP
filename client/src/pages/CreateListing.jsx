d406769dd10967c33fde395af110edf525975b27
Radhi Hudijan <radhif2009@gmail.com>
2024-10-21 00:07:08 +0200

    Refactor: Update CreateListing form layout and add image upload functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -4,7 +4,7 @@ function CreateListing() {
       <h1 className="text-3xl font-semibold text-center my-7">
         Create a Listing
       </h1>
-      <form className="flex flex-col sm:flex-row">
+      <form className="flex flex-col sm:flex-row gap-4">
         <div className=" flex flex-col gap-4 flex-1">
           <input
             type="text"
@@ -57,7 +57,7 @@ function CreateListing() {
             </div>
           </div>
 
-          <div className="flex gap-2">
+          <div className="flex gap-6 flex-wrap">
             <div className="flex gap-2 items-center">
               <input
                 type="number"
@@ -86,11 +86,12 @@ function CreateListing() {
               <input
                 type="number"
                 id="regularPrice"
-                min={1}
                 required
                 className=" p-3 rounded-lg border-gray-300"
+                min={1}
+                max={10}
               />
-              <div className="div">
+              <div className="flex flex-col items-center">
                 <span>Regular Price</span>
                 <span className="text-xs">($ /month)</span>
               </div>
@@ -101,15 +102,39 @@ function CreateListing() {
                 type="number"
                 id="discountedPrice"
                 min={1}
+                max={10}
                 required
                 className=" p-3 rounded-lg border-gray-300"
               />
-              <div className="div">
+              <div className="flex flex-col items-center">
                 <span>Discounted Price</span>
-                <span className="text-xs">($)</span>
+                <span className="text-xs">($ /month)</span>
               </div>
             </div>
           </div>
+        </div>
+        <div className="flex flex-col gap-4 flex-1">
+          <p className="font-semibold">
+            Images:
+            <span className="font-normal text-gray-600 ml-2">
+              The first image will be the cover (max 6)
+            </span>
+          </p>
+          <div className=" flex gap-4">
+            <input
+              type="file"
+              id="images"
+              accept="image/*"
+              multiple
+              className="p-3 border border-gray-300 rounded w-full"
+            />
+            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+              Upload
+            </button>
+          </div>
+          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80">
+            Create Listing
+          </button>
         </div>
       </form>
     </main>
