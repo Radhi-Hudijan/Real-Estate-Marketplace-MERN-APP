318e5911a65b8c32b36dbda83398f40f3aad1171
Radhi Hudijan <radhif2009@gmail.com>
2024-11-10 13:27:49 +0100

    Refactor: Add clipboard copy button for sharing listing link

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -8,6 +8,7 @@ import "swiper/css"
 import "swiper/css/navigation"
 
 import { Navigation } from "swiper/modules"
+import { FaMapMarkedAlt, FaShare } from "react-icons/fa"
 
 export default function Listing() {
   // get the listing id using the useParams hook
@@ -16,7 +17,8 @@ export default function Listing() {
   const [listing, setListing] = useState(null)
   const [error, setError] = useState(false)
   const [loading, setLoading] = useState(false)
-  console.log(listing)
+  const [copied, setCopied] = useState(false)
+
   // fetch the listing data
   useEffect(() => {
     // fetch the listing data
@@ -56,7 +58,7 @@ export default function Listing() {
         </Link>
       )}
       {listing && !loading && !error && (
-        <div className="w-full">
+        <div>
           <Swiper
             modules={[Navigation]}
             navigation
@@ -76,6 +78,51 @@ export default function Listing() {
               </SwiperSlide>
             ))}
           </Swiper>
+          {/* // create the clipboard copy button */}
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare
+              className=" text-slate-500"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href)
+                setCopied(true)
+                setTimeout(() => {
+                  setCopied(false)
+                }, 2000)
+              }}
+            />
+          </div>
+          {copied && (
+            <div className="fixed top-[19%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+              <p className=" text-slate-500">Link Copied</p>
+            </div>
+          )}
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+            <p className="text-2xl font-semibold">
+              {listing.name} - ${" "}
+              {listing.offer
+                ? listing.discountedPrice.toLocaleString("en-US")
+                : listing.regularPrice.toLocaleString("en-US")}
+              {listing.type === "rent" && "/month"}
+            </p>
+            <p className=" flex items-center mt-6 gap-2 text-slate-600 text-sm">
+              <FaMapMarkedAlt className="text-green-700" />
+              {listing.address}
+            </p>
+            <div className="flex gap-4">
+              <p className=" bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md ">
+                {listing.type === "rent" ? "For Rent" : "For Sale"}
+              </p>
+              {listing.offer && (
+                <p className=" bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md ">
+                  {`${Math.round(
+                    ((+listing.regularPrice - +listing.discountedPrice) /
+                      +listing.regularPrice) *
+                      100
+                  )}% Off`}{" "}
+                </p>
+              )}
+            </div>
+          </div>
         </div>
       )}
     </main>
